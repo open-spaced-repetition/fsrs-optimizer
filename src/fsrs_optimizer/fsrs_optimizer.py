c73ebad7c04d131284189cdad61e651fd0cb7483
Jarrett Ye <jarrett.ye@outlook.com>
2025-01-02 15:08:43 +0800

    Update fsrs_optimizer.py

--- src/fsrs_optimizer/fsrs_optimizer.py
+++ src/fsrs_optimizer/fsrs_optimizer.py
@@ -519,7 +519,6 @@ def fit_stability(delta_t, retention, size):
 class Optimizer:
     float_delta_t: bool = False
     enable_short_term: bool = True
-    
 
     def __init__(
         self, float_delta_t: bool = False, enable_short_term: bool = True
