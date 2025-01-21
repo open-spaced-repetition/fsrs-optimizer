fix formula again

src/fsrs_optimizer/fsrs_optimizer.py
@@ -390,12 +390,10 @@ class Trainer:
                 outputs, _ = self.model(sequences)
                 stabilities = outputs[seq_lens - 1, torch.arange(real_batch_size), 0]
                 retentions = power_forgetting_curve(delta_ts, stabilities)
-                normalized_retentions = torch.full_like(
-                    labels, labels.mean().clamp(0.0001, 0.9999)
-                )
-                normalized_cross_entropy = normalized_retentions * torch.log(
-                    retentions
-                ) + (1 - normalized_retentions) * torch.log(1 - retentions)
+                average_retention = retentions.mean().repeat(real_batch_size)
+                normalized_cross_entropy = average_retention * torch.log(retentions) + (
+                    1 - average_retention
+                ) * torch.log(1 - retentions)
                 loss = (
                     self.loss_fn(retentions, labels)
                     * weights
@@ -455,12 +453,10 @@ class Trainer:
                 outputs, _ = self.model(sequences.transpose(0, 1))
                 stabilities = outputs[seq_lens - 1, torch.arange(real_batch_size), 0]
                 retentions = power_forgetting_curve(delta_ts, stabilities)
-                normalized_retentions = torch.full_like(
-                    labels, labels.mean().clamp(0.0001, 0.9999)
-                )
-                normalized_cross_entropy = normalized_retentions * torch.log(
-                    retentions
-                ) + (1 - normalized_retentions) * torch.log(1 - retentions)
+                average_retention = retentions.mean().repeat(real_batch_size)
+                normalized_cross_entropy = average_retention * torch.log(retentions) + (
+                    1 - average_retention
+                ) * torch.log(1 - retentions)
                 loss = (
                     self.loss_fn(retentions, labels)
                     * weights